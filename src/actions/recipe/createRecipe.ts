a5fc2e66e428308a2f0d6f62caf507f4977edf01
mmkk <kk570@kent.ac.uk>
2025-10-20 22:01:44 +0100

    install ajv for schema management, create create recipe route and action

---
+++ src/actions/recipe/createRecipe.ts
@@ -0,0 +1,14 @@
+const { createRecipe: createRecipeInDB } = require('../db/functionHandler')
+import { type CreateRecipeInput } from '../../schema/Recipe/createRecipe'
+
+export const createRecipe = async (recipeData: CreateRecipeInput) => {
+  try {
+    const result = await createRecipeInDB(recipeData)
+    return {
+      status: 'success',
+      message: 'Recipe created successfully with ID: ' + result._id
+    }
+  } catch (error: Error | any) {
+    throw new Error('Error creating recipe: ' + error.message)
+  }
+}
--- src/routes/recipeRoute.ts
+++ src/routes/recipeRoute.ts
@@ -1,3 +1,12 @@
 const router = require('express').Router()
+const RecipeController = require('../controllers/recipeController')
+const { validateInputSchema } = require('../middleware/validationSchema')
+const { createRecipeSchema } = require('../schema/Recipe/createRecipe')
+
+router.post(
+  '/create',
+  validateInputSchema(createRecipeSchema),
+  RecipeController.createRecipe
+)
 
 module.exports = router
