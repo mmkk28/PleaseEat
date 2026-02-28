338ba3cb24e30ec15999f1ce4fd5245c14632385
mmkk <kk570@kent.ac.uk>
2026-02-28 16:15:54 +0000

    remove unnecessary validateOutputSchema

--- src/controllers/RecipeController.ts
+++ src/controllers/RecipeController.ts
@@ -1,5 +1,4 @@
 import { Error } from 'mongoose'
-import { validateOutputSchema } from '../schema/validationSchema'
 
 // Actions
 import { RecipeActions } from '../actions/recipe'
@@ -13,13 +12,6 @@ const createRecipe = async (req: any, res: any) => {
   try {
     const recipeData = req.body
     const result = await RecipeActions.createRecipe(recipeData)
-
-    if (!validateOutputSchema(createRecipeOutputSchema, result)) {
-      return res.status(500).json({
-        status: 'error',
-        message: 'Output validation failed'
-      })
-    }
     res.status(201).json(result)
   } catch (error: Error | any) {
     res.status(500).json({ status: 'error', message: error.message })
@@ -30,13 +22,6 @@ const getRecipe = async (req: any, res: any) => {
   try {
     const recipeId = req.params.id
     const result = await RecipeActions.getRecipe(recipeId)
-
-    if (!validateOutputSchema(getRecipeOutputSchema, result)) {
-      return res.status(500).json({
-        status: 'error',
-        message: 'Output validation failed'
-      })
-    }
     res.status(200).json(result)
   } catch (error: Error | any) {
     res.status(500).json({ status: 'error', message: error.message })
@@ -46,13 +31,6 @@ const getRecipe = async (req: any, res: any) => {
 const getAllRecipes = async (req: any, res: any) => {
   try {
     const result = await RecipeActions.getAllRecipe()
-
-    if (!validateOutputSchema(getRecipesOutputSchema, result)) {
-      return res.status(500).json({
-        status: 'error',
-        message: 'Output validation failed'
-      })
-    }
     res.status(200).json(result)
   } catch (error: Error | any) {
     res.status(500).json({ status: 'error', message: error.message })
